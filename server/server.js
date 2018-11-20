e5b9dec89b82b4b625af097ea1021c6211f9ad42
Travis Allen <travisallen6@gmail.com>
2018-11-20 16:07:40 -0700

    Change endpoint url path

--- server/server.js
+++ server/server.js
@@ -1,26 +1,21 @@
 require('dotenv').config();
-const   express = require('express')
-        , app = express()
-        , port = 3010;
-        
-        app.use(express.json())
-        
+const express = require('express'),
+  app = express(),
+  port = 3010;
+
+app.use(express.json());
+
 const aws = require('aws-sdk');
 
-const {
-    S3_BUCKET,
-    AWS_ACCESS_KEY_ID,
-    AWS_SECRET_ACCESS_KEY
-} = process.env
+const { S3_BUCKET, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } = process.env;
 
-app.get('/sign-s3', (req, res) => {
-
+app.get('/api/signs3', (req, res) => {
   aws.config = {
     region: 'us-west-1',
     accessKeyId: AWS_ACCESS_KEY_ID,
-    secretAccessKey: AWS_SECRET_ACCESS_KEY
-  }
-  
+    secretAccessKey: AWS_SECRET_ACCESS_KEY,
+  };
+
   const s3 = new aws.S3();
   const fileName = req.query['file-name'];
   const fileType = req.query['file-type'];
@@ -29,21 +24,21 @@ app.get('/sign-s3', (req, res) => {
     Key: fileName,
     Expires: 60,
     ContentType: fileType,
-    ACL: 'public-read'
+    ACL: 'public-read',
   };
 
   s3.getSignedUrl('putObject', s3Params, (err, data) => {
-    if(err){
+    if (err) {
       console.log(err);
       return res.end();
     }
     const returnData = {
       signedRequest: data,
-      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`,
     };
 
-    return res.send(returnData)
+    return res.send(returnData);
   });
 });
 
-app.listen(port, () => console.log(`Hard to port ${port}`))
+app.listen(port, () => console.log(`Hard to port ${port}`));
--- src/App.js
+++ src/App.js
@@ -1,75 +1,79 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
-import {v4 as randomString} from 'uuid'
-import Dropzone from 'react-dropzone'
-import { GridLoader } from 'react-spinners'
+import { v4 as randomString } from 'uuid';
+import Dropzone from 'react-dropzone';
+import { GridLoader } from 'react-spinners';
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       isUploading: false,
-      images: [],
       url: 'http://via.placeholder.com/450x450',
-      value: ''                
-    }
+    };
   }
-  
+
   getSignedRequest = ([file]) => {
-    this.setState({isUploading: true})
+    this.setState({ isUploading: true });
     // We are creating a file name that consists of a random string, and the name of the file that was just uploaded with the spaces removed and hyphens inserted instead. This is done using the .replace function with a specific regular expression. This will ensure that each file uploaded has a unique name which will prevent files from overwriting other files due to duplicate names.
-    const fileName = `${randomString()}-${file.name.replace(/\s/g, '-')}`
+    const fileName = `${randomString()}-${file.name.replace(/\s/g, '-')}`;
 
     // We will now send a request to our server to get a "signed url" from Amazon. We are essentially letting AWS know that we are going to upload a file soon. We are only sending the file-name and file-type as strings. We are not sending the file itself at this point.
-    axios.get('/sign-s3', {
-      params: {
-        'file-name': fileName,
-        'file-type': file.type
-      }
-    }).then( (response) => {
-      const { signedRequest, url } = response.data 
-      this.uploadFile(file, signedRequest, url)
-    }).catch( err => {
-      console.log(err)
-    })
-  }
+    axios
+      .get('/api/signs3', {
+        params: {
+          'file-name': fileName,
+          'file-type': file.type,
+        },
+      })
+      .then(response => {
+        const { signedRequest, url } = response.data;
+        this.uploadFile(file, signedRequest, url);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
   uploadFile = (file, signedRequest, url) => {
-    
-    var options = {
+    const options = {
       headers: {
-        'Content-Type': file.type
-      }
+        'Content-Type': file.type,
+      },
     };
 
-    axios.put(signedRequest, file, options)
-    .then( response => {
-      this.setState({isUploading: false, url: url})
-      // THEN DO SOMETHING WITH THE URL. SEND TO DB USING POST REQUEST OR SOMETHING
-    })
-    .catch( err => {
-      this.setState({
-        isUploading: false
+    axios
+      .put(signedRequest, file, options)
+      .then(response => {
+        this.setState({ isUploading: false, url });
+        // THEN DO SOMETHING WITH THE URL. SEND TO DB USING POST REQUEST OR SOMETHING
       })
-      if(err.response.status === 403) {
-        alert('Your request for a signed URL failed with a status 403. Double check the CORS configuration and bucket policy in the README. You also will want to double check your AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY in your .env and ensure that they are the same as the ones that you created in the IAM dashboard. You may need to generate new keys\n' + err.stack)
-      } else {
-        alert(`ERROR: ${err.status}\n ${err.stack}`)
-      }
-    })
-  }
+      .catch(err => {
+        this.setState({
+          isUploading: false,
+        });
+        if (err.response.status === 403) {
+          alert(
+            `Your request for a signed URL failed with a status 403. Double check the CORS configuration and bucket policy in the README. You also will want to double check your AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY in your .env and ensure that they are the same as the ones that you created in the IAM dashboard. You may need to generate new keys\n${
+              err.stack
+            }`
+          );
+        } else {
+          alert(`ERROR: ${err.status}\n ${err.stack}`);
+        }
+      });
+  };
 
   render() {
-    
+    const { url, isUploading } = this.state;
     return (
       <div className="App">
-
         <h1>Upload</h1>
-        <h1>{this.state.url}</h1>
-        <img src={ this.state.url } alt='' width='450px' />        
+        <h1>{url}</h1>
+        <img src={url} alt="" width="450px" />
 
-        <Dropzone 
+        <Dropzone
           onDropAccepted={this.getSignedRequest}
           style={{
             position: 'relative',
@@ -85,13 +89,10 @@ class App extends Component {
             alignItems: 'center',
             fontSize: 28,
           }}
-          accept='image/*'
-          multiple={false} >
-            
-            { this.state.isUploading 
-              ?  <GridLoader />
-              : <p>Drop File or Click Here</p>
-            }
+          accept="image/*"
+          multiple={false}
+        >
+          {isUploading ? <GridLoader /> : <p>Drop File or Click Here</p>}
         </Dropzone>
       </div>
     );
